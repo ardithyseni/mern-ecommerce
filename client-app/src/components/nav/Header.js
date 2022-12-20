a0917789962b67a61698e802cd905075a313e6df
ardithyseni <ah49393@ubt-uni.net>
2022-12-20 14:11:07 +0100

    navigation put

---
+++ client-app/src/components/nav/Header.js
@@ -0,0 +1,67 @@
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import React, { useState } from 'react';
+
+const items = [
+    {
+        label: 'Navigation One',
+        key: 'mail',
+        icon: <MailOutlined />,
+    },
+    {
+        label: 'Navigation Two',
+        key: 'app',
+        icon: <AppstoreOutlined />,
+        disabled: true,
+    },
+    {
+        label: 'Navigation Three - Submenu',
+        key: 'SubMenu',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                type: 'group',
+                label: 'Item 1',
+                children: [
+                    {
+                        label: 'Option 1',
+                        key: 'setting:1',
+                    },
+                    {
+                        label: 'Option 2',
+                        key: 'setting:2',
+                    },
+                ],
+            },
+            {
+                type: 'group',
+                label: 'Item 2',
+                children: [
+                    {
+                        label: 'Option 3',
+                        key: 'setting:3',
+                    },
+                    {
+                        label: 'Option 4',
+                        key: 'setting:4',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        label: 'Navigation Four',
+        key: 'four',
+        icon: <MailOutlined />,
+    },
+];
+const Header = () => {
+    const [current, setCurrent] = useState('');
+
+    const handleClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+    return <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+export default Header;
