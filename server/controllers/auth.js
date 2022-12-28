339e1c291f83d2b559543f2f087f2fbd0f65b4f7
ardithyseni <ah49393@ubt-uni.net>
2022-12-28 16:00:58 +0100

    fist controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,7 @@
+
+
+exports.createOrUpdateUser = (req, res) => {
+    res.json({
+        data: "hey you hit create-or-update-user api endpoint",
+    });
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/create-or-update-user", (req, res) => {
-    res.json({
-        data: "hey you hit create-or-update-user api endpoint",
-    });
-});
+const { createOrUpdateUser } = require("../controllers/auth");
+
+router.get("/create-or-update-user", createOrUpdateUser);
 
 module.exports = router;
