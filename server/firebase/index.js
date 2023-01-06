bff57794ee2bd1ae2e616711e1b8801237928ba2
ardithyseni <ah49393@ubt-uni.net>
2023-01-06 16:11:22 +0100

    working on firebase backend

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -1,7 +1,5 @@
-
-import * as admin from 'firebase-admin';
-
-import fbServiceAccountKey from '../config/fbServiceAccountKey.json'
+import admin from 'firebase-admin';
+import fbServiceAccountKey from '../config/fbServiceAccountKey.json'  assert { type: 'json' };
 
 admin.initializeApp({
   credential: admin.credential.cert(fbServiceAccountKey)
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,9 +1,19 @@
-// import admin  from '../firebase/index';
+import admin from '../firebase/index.js';
 
 // https://expressjs.com/en/guide/writing-middleware.html
 
-export const authCheck = (req, res, next) => {
-    console.log(req.headers); // token request
+export const authCheck = async (req, res, next) => {
+    // console.log(req.headers); // token request
+    try {
+        const firebaseUser = await admin.auth()
+            .verifyIdToken(req.headers.authToken); // match from Login.js frontend
+        console.log('Firebase user in authcheck: \n', firebaseUser);
+    } catch (error) {
+        console.log('Error authcheck middleware method: ', error);
+        res.status(401).json({
+            error: 'Invalid or expired token',
+        })
+    }
     // next();
 }
 
