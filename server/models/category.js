aef94770250f713d0b7f65559020447b66953b4a
ardithyseni <ah49393@ubt-uni.net>
2023-01-21 14:23:34 +0100

    added routes for category crud

---
+++ server/models/category.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+// OBJECTID
+
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: "Category name is required",
+            minLength: [3, "Category name too short"],
+            maxLength: [32, "Category name too long"],
+        },
+        slug: {
+            type: String,
+            unique: true,
+            lowercase: true,
+            index: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Category", categorySchema);
---
+++ server/routes/category.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const router = express.Router();
+// controllers
+import { createCategory, readCategory, updateCategory, removeCategory, listCategories } from "../controllers/category.js";
+
+// middlewares
+import { authCheck, adminCheck } from '../middlewares/auth.js';
+
+router.post('/category', authCheck, adminCheck, createCategory);
+router.get('/categories', listCategories);
+router.get('/category/:slug', authCheck, adminCheck, readCategory);
+router.put('/category/:slug', authCheck, adminCheck, updateCategory);
+router.delete('/category/:slug', authCheck, adminCheck, removeCategory);
+
+export default router;
