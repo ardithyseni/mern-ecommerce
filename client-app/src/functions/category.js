77d026ad349303fd2bd4c115107ecbb97d207db4
ardithyseni <ah49393@ubt-uni.net>
2023-01-25 14:56:33 +0100

    create category working front&back

---
+++ client-app/src/functions/category.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export const getCategories = async () => {
+    return await axios.get(`${process.env.REACT_APP_API}/categories`);
+};
+
+export const getCategory = async (slug) => {
+    return await axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
+};
+
+export const removeCategory = async (slug, idtoken) => {
+    return await axios.delete(`${process.env.REACT_APP_API}/category/${slug}`, {
+      headers: {
+        idtoken
+      }  
+    });
+};
+
+export const updateCategory = async (slug, category, idtoken) => {
+    return await axios.put(`${process.env.REACT_APP_API}/category/${slug}`, {
+      headers: {
+        idtoken
+      }  
+    });
+};
+
+export const createCategory = async (category, idtoken) => {
+    return await axios.post(`${process.env.REACT_APP_API}/category`, category, {
+      headers: {
+        idtoken
+      }  
+    });
+};
+
--- client-app/src/pages/admin/category/CategoryCreate.js
+++ client-app/src/pages/admin/category/CategoryCreate.js
@@ -1,18 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminNav from '../../../components/nav/AdminNav'
+import { toast } from 'react-toastify'
+import { useSelector } from 'react-redux'
+import {
+    createCategory,
+    getCategories,
+    removeCategory
+} from '../../../functions/category'
+import { Button, Checkbox, Form, Input } from 'antd';
+import { CheckOutlined, LoadingOutlined } from '@ant-design/icons'
+import create from '@ant-design/icons/lib/components/IconFont'
+
 
 const CategoryCreate = () => {
+
+    const { user } = useSelector(state => ({ ...state }))
+
+    const [name, setName] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        createCategory({ name }, user.token)
+            .then(res => {
+
+                console.log(res)
+                setLoading(false)
+                setName('')
+                toast.success(`Category: ${res.data.name} created!`)
+
+            }).catch(err => {
+
+                console.log(err);
+                setLoading(false)
+                toast.error(err.response.data)
+                
+            })
+    }
+
+    const errorSubmit = (e) => {
+        e.preventDefault();
+        toast.error("Create category failed")
+    }
+
+    const categoryForm = () => (
+
+        <form onSubmit={handleSubmit}>
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Category Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    autoFocus
+                    required
+                />
+            </div>
+
+            <br />
+            <Button
+                onClick={handleSubmit}
+                type="primary"
+                block
+                size="large"
+                shape="round"
+                icon={loading ? <LoadingOutlined /> : <CheckOutlined />}
+                className="mb-2"
+            // disabled={!email || password.length < 6}
+            >
+                Save
+            </Button>
+        </form>
+    )
+
     return (
         <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-2">
-              <AdminNav />
+            <div className="row">
+                <div className="col-md-2">
+                    <AdminNav />
+                </div>
+                <div className="col">
+                    <h4>Create category</h4>
+                    {categoryForm()}
+                </div>
             </div>
-            <div className="col">Category Create</div>
-          </div>
         </div>
-    
-      )
+
+    )
 }
 
 export default CategoryCreate
