e89ee9557963d6e9b777fed80e9a3dcd2ac190c2
ardithyseni <ah49393@ubt-uni.net>
2023-01-27 14:09:41 +0100

    search category

--- client-app/src/pages/admin/category/CategoryCreate.js
+++ client-app/src/pages/admin/category/CategoryCreate.js
@@ -8,7 +8,7 @@ import {
     getCategories,
     removeCategory
 } from '../../../functions/category'
-import { Button, Space, Table, Tag } from 'antd';
+import { Button, Input } from 'antd';
 import { CheckOutlined, DeleteOutlined, EditOutlined, LoadingOutlined } from '@ant-design/icons'
 import CategoryForm from '../../../components/forms/CategoryForm'
 
@@ -19,35 +19,9 @@ const CategoryCreate = () => {
     const [name, setName] = useState("");
     const [loading, setLoading] = useState(false);
     const [categories, setCategories] = useState([]);
-    const [columns, setColumns] = useState([
-        {
-            title: "Name",
-            dataIndex: "name",
-            key: 'name',
-            width: "80%"
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (_, c) => (
-                <Space size="middle">
-                    <Link to={`/admin/category/${c.slug}`}>
-                        <EditOutlined style={{ fontSize: '20px' }} />
-                    </Link>
-                    <span onClick={() => handleRemoveCategory(c.slug)} className='btn btn-sm'>
-                        <DeleteOutlined style={{ fontSize: '20px' }} className='text-danger' />
-                    </span>
-                </Space>
-            ),
-        }
-    ]);
-    const [tabledata, setTabledata] = useState([]);
-    const [action, setAction] = useState([
-        {
-
-        }
-    ])
-
+    // search & filter step 1
+    const [keyword, setKeyword] = useState("");
+    const { Search } = Input;
     useEffect(() => {
         loadCategories();
     }, [])
@@ -95,37 +69,44 @@ const CategoryCreate = () => {
         }
     }
 
-    const categoryForm = () => (
+    const handleSearchInput = (e) => {
+        e.preventDefault();
+        setKeyword(e.target.value.toLowerCase());
+    }
 
-        <form onSubmit={handleSubmit}>
+    const searched = (keyword) => (c) => c.name.toLowerCase().includes(keyword)
 
-            <div className="form-group">
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Category Name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    autoFocus
-                    required
-                />
-            </div>
+    // const categoryForm = () => (
 
-            <br />
-            <Button
-                onClick={handleSubmit}
-                type="primary"
-                block
-                size="large"
-                shape="round"
-                icon={loading ? <LoadingOutlined /> : <CheckOutlined />}
-                className="mb-2"
-                disabled={name.length < 2}
-            >
-                Save
-            </Button>
-        </form>
-    )
+    //     <form onSubmit={handleSubmit}>
+
+    //         <div className="form-group">
+    //             <input
+    //                 type="text"
+    //                 className="form-control"
+    //                 placeholder="Category Name"
+    //                 value={name}
+    //                 onChange={(e) => setName(e.target.value)}
+    //                 autoFocus
+    //                 required
+    //             />
+    //         </div>
+
+    //         <br />
+    //         <Button
+    //             onClick={handleSubmit}
+    //             type="primary"
+    //             block
+    //             size="large"
+    //             shape="round"
+    //             icon={loading ? <LoadingOutlined /> : <CheckOutlined />}
+    //             className="mb-2"
+    //             disabled={name.length < 2}
+    //         >
+    //             Save
+    //         </Button>
+    //     </form>
+    // )
 
     return (
         <div className="container-fluid">
@@ -146,7 +127,11 @@ const CategoryCreate = () => {
                             />
                         </div>
                     </div>
+
+
                     <hr />
+                    <Search value={keyword} onChange={handleSearchInput} placeholder="Search Category" enterButton="Search" size="large" style={{ margin: "10px 0px 10px 0px" }} />
+                    
                     <table className='table table-hover w-auto mt-1'>
                         <thead className="thead-light">
                             <tr>
@@ -155,7 +140,7 @@ const CategoryCreate = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {categories.map((c) => (
+                            {categories.filter(searched(keyword)).map((c) => (
                                 <tr key={c._id}>
                                     <td>{c.name}</td>
 
