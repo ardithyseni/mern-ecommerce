a4acce6083f00e3d0edf6db7b24ac2106a4ebaba
ardithyseni <ah49393@ubt-uni.net>
2023-02-08 00:00:56 +0100

    popup showing on delete

--- client-app/src/components/cards/AdminProductCard.js
+++ client-app/src/components/cards/AdminProductCard.js
@@ -1,37 +1,56 @@
-import React from 'react'
-import { Avatar, Card } from 'antd';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import React from "react";
+import { Modal, Card, Button, Popconfirm } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 const { Meta } = Card;
 
-const AdminProductCard = ({ product, loading, handleRemoveProduct, handleCancelModal, showModal }) => {
-
+const AdminProductCard = ({
+    open,
+    product,
+    loading,
+    handleRemoveProduct,
+    handleCancelModal,
+    showModal,
+}) => {
     //destructuring the properties
-    const { title, description, images, slug } = product
+    const { title, description, images, slug } = product;
     return (
-        <Card
-            style={{
-                width: 300,
-            }}
-            cover={
-                <img
-                    src={images && images.length ? images[0].url : 'https://via.placeholder.com/300x200'}
-                    className="p-1"
+        <>
+            <Card
+                style={{
+                    width: 300,
+                }}
+                cover={
+                    <img
+                        src={
+                            images && images.length
+                                ? images[0].url
+                                : "https://via.placeholder.com/300x200"
+                        }
+                        className="p-1"
+                    />
+                }
+                actions={[
+                    <EditOutlined key="edit" />,
+                    <Popconfirm
+                        title="Delete this product?"
+                        onConfirm={handleRemoveProduct}
+                        // onCancel={cancel}
+                        okText="Yes"
+                        cancelText="No">
+                        <DeleteOutlined type="button" key="delete"/>,
+                    </Popconfirm>,
+                ]}
+            >
+                <Meta
+                    // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                    title={title}
+                    description={`${description && description.substring(0, 50)
+                        }...`}
                 />
-            }
-            actions={[
-                <EditOutlined key="edit" />,
-                <DeleteOutlined key="delete" onClick={() => {
-                    showModal()
-                    handleRemoveProduct(slug)}} />,
-            ]}
-        >
-            <Meta
-                // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                title={title}
-                description={`${description && description.substring(0, 45)}...`}
-            />
-        </Card>
-    )
-}
+            </Card>
+
+        </>
+    );
+};
 
 export default AdminProductCard;
--- client-app/src/pages/admin/product/AllProducts.js
+++ client-app/src/pages/admin/product/AllProducts.js
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { getProductsByCount, deleteProduct } from "../../../functions/product";
 import AdminProductCard from "../../../components/cards/AdminProductCard";
-import { Modal, Button } from "antd";
+import { Modal, Button, Spin } from "antd";
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     loadAllProducts();
@@ -26,66 +25,51 @@ const AllProducts = () => {
       });
   };
 
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
+  // const showModal = () => {
+  //   setOpen(true);
+  //   console.log("show modal triggered", open);
+  // };
 
-  const handleCancelModal = () => {
-    setIsModalOpen(false);
-  };
+  // const handleCancelModal = () => {
+  //   setOpen(false);
+  // };
 
   const handleRemoveProduct = (slug) => {
     console.log("send delete request", slug);
   };
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-2">
-          <AdminNav />
-        </div>
+    <>
 
-        <div className="col">
-          {loading ? (
-            <h4 className="text-danger">Loading...</h4>
-          ) : (
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
+            <AdminNav />
+          </div>
+
+          <div className="col">
             <h4>All Products</h4>
-          )}
-          <div className="row d-flex flex-wrap">
-            {products.map((product) => (
-              <div
-                key={product._id}
-                className="col-md-4 mb-4 mt-4"
-              >
-                <AdminProductCard
-                  product={product}
-                  loading={loading}
-                  handleRemoveProduct={handleRemoveProduct}
-                  handleCancelModal={handleCancelModal}
-                  showModal={showModal}
-                />
+            
+              <div className="row d-flex flex-wrap">
+                {products.map((product) => (
+                  <div
+                    key={product._id}
+                    className="col-md-4 mb-4 mt-4"
+                  >
+                    <AdminProductCard
+                      product={product}
+                      loading={loading}
+                      handleRemoveProduct={handleRemoveProduct}
+                    />
+
+                  </div>
+                ))}
               </div>
-            ))}
-            <Modal
-              open={isModalOpen}
-              title="Delete product?"
-              onOk={handleRemoveProduct}
-              onCancel={handleCancelModal}
-              footer={[
-                <Button key="back" onClick={handleCancelModal}>
-                  Back
-                </Button>,
-                <Button key="submit" type="primary" loading={loading} onClick={handleRemoveProduct}>
-                  Submit
-                </Button>,
-              ]}
-            >
-        
-            </Modal>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
