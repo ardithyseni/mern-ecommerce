afea1ae65a39b484cad0d1d76b9077c9f7e1843f
ardithyseni <ah49393@ubt-uni.net>
2023-03-07 16:53:05 +0100

    product update route and controller

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,63 +2,81 @@ import Product from "../models/product.js";
 import slugify from "slugify";
 
 export const createProduct = async (req, res) => {
-  try {
-    console.log(req.body);
-    req.body.slug = slugify(req.body.title);
-    const newProduct = await new Product(req.body).save();
-    res.json(newProduct);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      err: err.message,
-    });
-  }
+    try {
+        console.log(req.body);
+        req.body.slug = slugify(req.body.title);
+        const newProduct = await new Product(req.body).save();
+        res.json(newProduct);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            err: err.message,
+        });
+    }
 };
 
 export const listProducts = async (req, res) => {
-  let products = await Product.find({});
-  // .limit(parseInt(req.params.count))
-  // .populate('category')
-  // .populate('subcategories')
-  // .sort([['createdAt', 'desc']])
-  // .exec();
-  res.json(products);
+    let products = await Product.find({});
+    // .limit(parseInt(req.params.count))
+    // .populate('category')
+    // .populate('subcategories')
+    // .sort([['createdAt', 'desc']])
+    // .exec();
+    res.json(products);
 };
 
 export const listProductsByCount = async (req, res) => {
-  let products = await Product.find({})
-    .limit(parseInt(req.params.count))
-    .populate("category")
-    .populate("subcategories")
-    .sort([["createdAt", "desc"]])
-    .exec();
-  res.json(products);
+    let products = await Product.find({})
+        .limit(parseInt(req.params.count))
+        .populate("category")
+        .populate("subcategories")
+        .sort([["createdAt", "desc"]])
+        .exec();
+    res.json(products);
 };
 
 export const removeProduct = async (req, res) => {
-  // console.log(req);
-  try {
-    const product = await Product.findOne({
-      slug: req.params.slug,
-    }).exec();
+    // console.log(req);
+    try {
+        const product = await Product.findOne({
+            slug: req.params.slug,
+        }).exec();
 
-    if (!product) {
-      return res.status(404).send("Product not found");
+        if (!product) {
+            return res.status(404).send("Product not found");
+        }
+
+        const deletedProduct = await product.remove();
+        res.json(deletedProduct);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send("Product remove controller failed");
     }
-
-    const deletedProduct = await product.remove();
-    res.json(deletedProduct);
-  } catch (err) {
-    console.log(err);
-    return res.status(400).send("Product remove controller failed");
-  }
 };
 
 export const getBySlug = async (req, res) => {
-  const product = await Product.findOne({ slug: req.params.slug })
-    .populate("category")
-    .populate("subcategories")
-    .exec();
+    const product = await Product.findOne({ slug: req.params.slug })
+        .populate("category")
+        .populate("subcategories")
+        .exec();
 
-  res.json(product);
+    res.json(product);
+};
+
+export const updateProduct = async (req, res) => {
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title);
+        }
+        const updatedProduct = await Product.findByIdAndUpdate(
+            { slug: req.params.slug },
+            req.body,
+            { new: true }
+        ).exec();
+
+        res.json(updatedProduct);
+    } catch (error) {
+        console.log("update product error", error);
+        return res.status(400).send("product update failed");
+    }
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,15 +1,23 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 // controllers
-import { createProduct, listProducts, getBySlug, listProductsByCount, removeProduct } from "../controllers/product.js";
+import {
+    createProduct,
+    updateProduct,
+    listProducts,
+    getBySlug,
+    listProductsByCount,
+    removeProduct,
+} from "../controllers/product.js";
 
 // middlewares
-import { authCheck, adminCheck } from '../middlewares/auth.js';
+import { authCheck, adminCheck } from "../middlewares/auth.js";
 
-router.post('/product', authCheck, adminCheck, createProduct);
-router.get('/products', listProducts);
-router.get('/products/:count', listProductsByCount);
-router.delete('/product/:slug', authCheck, adminCheck, removeProduct);
-router.get('/product/:slug', getBySlug);
+router.post("/product", authCheck, adminCheck, createProduct);
+router.get("/products", listProducts);
+router.get("/products/:count", listProductsByCount);
+router.delete("/product/:slug", authCheck, adminCheck, removeProduct);
+router.get("/product/:slug", getBySlug);
+router.put("/product/:slug", authCheck, adminCheck, updateProduct);
 
 export default router;
