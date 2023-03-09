35b2e7a7c5789eb608266a44e806c1a7051824f5
ardithyseni <ah49393@ubt-uni.net>
2023-03-09 19:01:28 +0100

    product details done for now

--- client-app/src/pages/ProductDetails.js
+++ client-app/src/pages/ProductDetails.js
@@ -1,49 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { getProductBySlug } from '../functions/product';
+import React, { useEffect, useState } from "react";
+import { getProductBySlug } from "../functions/product";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import { Card, Skeleton, Badge, Descriptions, Tag } from 'antd';
-import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Carousel } from "react-responsive-carousel";
+import { Card, Skeleton, Tabs, Descriptions, Tag } from "antd";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 const { Meta } = Card;
-
 
 const ProductDetails = ({ match }) => {
     const [product, setProduct] = useState();
     const [loading, setLoading] = useState(true);
 
-    const { title, description, images, shipping, color, brand, subcategories, category, price } = product || {};
+    const {
+        title,
+        description,
+        images,
+        shipping,
+        color,
+        brand,
+        subcategories,
+        category,
+        price,
+    } = product || {};
 
     const { slug } = match.params;
 
     useEffect(() => {
         loadProduct();
-    }, [slug])
+    }, [slug]);
 
     const loadProduct = () => {
         setLoading(true);
         getProductBySlug(slug).then((res) => {
             setProduct(res.data);
             setLoading(false);
-        })
-    }
+        });
+    };
 
     if (loading) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
 
     return (
-        <div className='container-fluid px-5'>
-            {JSON.stringify(subcategories)}
+        <div className="container-fluid px-5">
+            {/* {JSON.stringify(subcategories)} */}
             {/* Product image and details  START*/}
-            <div className='row pt-4'>
-                <div className='col-md-6'>
-                    <Carousel showArrows={true} style={{ border: '1px solid lightgray' }}>
-                        {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+            <div className="row pt-4">
+                <div className="col-md-6">
+                    <Carousel
+                        showArrows={true}
+                        style={{ border: "1px solid lightgray" }}
+                    >
+                        {images &&
+                            images.map((i) => (
+                                <img src={i.url} key={i.public_id} />
+                            ))}
                     </Carousel>
-                </div>
-                <div className='col-md-6'>
 
+                    <Tabs
+                        style={{
+                            border: "1px solid rgb(240, 240, 240)",
+                            padding: "8px",
+                            minHeight: "200px",
+                        }}
+                    >
+                        <Tabs.TabPane tab="Description" key="description">
+                            {description}
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab="More info" key="more-info">
+                            For support contact ardit.hys@outlook.com
+                        </Tabs.TabPane>
+                    </Tabs>
+                </div>
+                <div className="col-md-6">
                     {/* <ul className="list-group list-group-flush">
                         <li className="list-group-item">Cras justo odio</li>
                         <li className="list-group-item">Dapibus ac facilisis in</li>
@@ -52,16 +81,34 @@ const ProductDetails = ({ match }) => {
                         <li className="list-group-item">Vestibulum at eros</li>
                     </ul> */}
                     <Descriptions title={title} bordered>
+                        <Descriptions.Item span={3} label="Order time">
+                            2018-04-24 18:00:00
+                        </Descriptions.Item>
 
-                        <Descriptions.Item span={3} label="Order time">2018-04-24 18:00:00</Descriptions.Item>
-                        <Descriptions.Item span={3} label="Shipping">{shipping}</Descriptions.Item>
-                        <Descriptions.Item span={3} label="Category">{category.name}</Descriptions.Item>
-                        <Descriptions.Item span={3} label="Subcategories">{subcategories.map((s) => (
-                            <Tag key={s._id}>
-      <a href="#">{s.name}</a>
-    </Tag>
-                        ))}</Descriptions.Item>
-                        <Descriptions.Item span={3} label="Order time">2018-04-24 18:00:00</Descriptions.Item>
+                        <Descriptions.Item span={3} label="Category">
+                            <Tag>
+                                <a href="#">{category.name}</a>
+                            </Tag>
+                        </Descriptions.Item>
+                        <Descriptions.Item span={3} label="Subcategories">
+                            {subcategories.map((s) => (
+                                <Tag key={s._id}>
+                                    <a href="#">{s.name}</a>
+                                </Tag>
+                            ))}
+                        </Descriptions.Item>
+                        <Descriptions.Item span={3} label="Brand">
+                            {brand}
+                        </Descriptions.Item>
+                        <Descriptions.Item span={3} label="Color">
+                            {color}
+                        </Descriptions.Item>
+                        <Descriptions.Item span={3} label="Shipping">
+                            {shipping}
+                        </Descriptions.Item>
+                        <Descriptions.Item span={3} label="Price">
+                            <b>{price} €</b>
+                        </Descriptions.Item>
                         {/* <Descriptions.Item label="Usage Time" span={2}>
                             2019-04-24 18:00:00
                         </Descriptions.Item>
@@ -88,18 +135,28 @@ const ProductDetails = ({ match }) => {
                     <Card
                         actions={[
                             <>
-                                <ShoppingCartOutlined style={{ fontSize: '23px' }} type="button" key="cart" />
+                                <ShoppingCartOutlined
+                                    className="text-success"
+                                    style={{ fontSize: "23px" }}
+                                    type="button"
+                                    key="cart"
+                                />
                                 <br />
                                 Add to Cart
                             </>,
                             <Link to="/">
-                                <HeartOutlined className='text-danger' style={{ fontSize: '23px' }} type="button" key="heart" />
+                                <HeartOutlined
+                                    className="text-danger"
+                                    style={{ fontSize: "23px" }}
+                                    type="button"
+                                    key="heart"
+                                />
                                 <br />
                                 Add to Wishlist
-                            </Link>
-                        ]}>
+                            </Link>,
+                        ]}
+                    >
                         <Skeleton loading={loading}>
-
                             {/* <Meta
                             // title={title}
                             // description={description}
@@ -110,11 +167,15 @@ const ProductDetails = ({ match }) => {
                 </div>
             </div>
             {/* Product image and details  END*/}
-            <div className='row'>
-                <div>Related products</div>
+            <div className="row p-5">
+                <div className="col text-center pt-5 pb-5">
+                    <hr />
+                    <h4>Related Products</h4>
+                    <hr />
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default ProductDetails;
