0af2413ee46ec2fac6bc08e3de897c180618f837
ardithyseni <ah49393@ubt-uni.net>
2023-03-17 16:23:57 +0100

    search box text updating in redux state

--- client-app/src/components/nav/Header.js
+++ client-app/src/components/nav/Header.js
@@ -11,6 +11,7 @@ import { Link, useHistory } from "react-router-dom";
 // import { auth } from "../../firebase";
 import { getAuth } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
+import Search from "../forms/Search";
 
 const { SubMenu, Item } = Menu;
 
@@ -72,6 +73,9 @@ const Header = () => {
           <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
         </SubMenu>
       )}
+      <span className="float-right p-1">
+        <Search />
+      </span>
     </Menu>
   );
 };
