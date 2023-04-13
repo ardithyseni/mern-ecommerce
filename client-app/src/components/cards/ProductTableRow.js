5bcccb0f1b046457367e89b6e2138c7b77bd0291
ardithyseni <ah49393@ubt-uni.net>
2023-04-13 14:49:16 +0200

    itemcount linked with product quantity

--- client-app/src/components/cards/ProductTableRow.js
+++ client-app/src/components/cards/ProductTableRow.js
@@ -1,20 +1,69 @@
-import React from 'react'
+import React from "react";
+import ModalImage from "react-modal-image";
+import { useDispatch } from "react-redux";
+import { toast } from 'react-toastify';
 
-const ProductTableRow = ({p}) => {
+const ProductTableRow = ({ p }) => {
+
+  const dispatch = useDispatch();
+
+  const handleCountChange = (e) => {
+
+    let itemCount = e.target.value < 1 ? 1 : e.target.value;
+
+    if (itemCount > p.quantity) {
+      toast.error(`Max available quantity: ${p.quantity}`);
+      return;
+    }
+
+    let cart = [];
+
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      cart.map((product, i) => {
+        if (product._id === p._id) {
+          cart[i].count = itemCount;
+        }
+      });
+
+      localStorage.setItem('cart', JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart
+      })
+    }
+  };
+
+
   return (
     <tr>
-        <td>
-            Image
-        </td>
-        <td>{p.title}</td>
-        <td>{p.price}</td>
-        <td>{p.brand}</td>
-        <td>{p.color}</td>
-        <td>{p.count}</td>
-        <td>{p.shipping}</td>
-        <td>Delete Icon</td>
+      <td>
+        <div style={{ width: "100px", height: "auto" }}>
+          {p.images.length ? (
+            <ModalImage small={p.images[0].url} large={p.images[0].url} />
+          ) : (
+            "No image found"
+          )}
+        </div>
+      </td>
+      <td>{p.title}</td>
+      <td>{p.price}</td>
+      <td>{p.brand}</td>
+      <td>{p.color}</td>
+      <td className="text-center">
+        <input
+          type="number"
+          className="form-control"
+          value={p.count}
+          onChange={handleCountChange}
+        />
+      </td>
+      <td>{p.shipping}</td>
+      <td>Delete Icon</td>
     </tr>
-  )
-}
+  );
+};
 
-export default ProductTableRow
+export default ProductTableRow;
