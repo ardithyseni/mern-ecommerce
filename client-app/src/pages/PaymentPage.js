8c33a3a6762600c89197d9f15b51271e97025573
ardithyseni <ah49393@ubt-uni.net>
2023-04-24 22:24:26 +0200

    stripe initialized front-end

--- client-app/src/pages/PaymentPage.js
+++ client-app/src/pages/PaymentPage.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
+import StripeCheckout from '../components/forms/StripeCheckout';
+import '../stripe.css';
+
+// Load stripe outside of components render 
+// to avoid recreating stripe object on every render
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 const PaymentPage = () => {
   return (
     <div className='container p-5 text-center'>
-      <p>Complete your purchase</p>
+      <h4>Complete your payment</h4>
+      <Elements stripe={stripePromise}>
+        <div className='col-md-8 offset-md-2'>
+          <StripeCheckout />
+        </div>
+      </Elements>
     </div>
   )
 }
