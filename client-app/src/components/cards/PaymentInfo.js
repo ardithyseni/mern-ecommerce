9a8bcb53a4c06557092f1d1e225a679efdb2850a
ardithyseni <ah49393@ubt-uni.net>
2023-04-29 21:13:40 +0200

    purhcase history looking good

---
+++ client-app/src/components/cards/PaymentInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PaymentInfo = ({ order }) => {
+    return (
+        <div>
+            <p className='mb-3'>Order ID: {order.paymentIntent.id}</p>
+            <h6>Amount: {(order.paymentIntent.amount /= 100).toLocaleString("en-DE", { style: "currency", currency: "EUR" })}</h6>
+            <h6>Order Status: <span className='badge badge-light'>{order.orderStatus}</span></h6>
+        </div>
+    )
+}
+
+export default PaymentInfo
--- client-app/src/pages/user/UserHistory.js
+++ client-app/src/pages/user/UserHistory.js
@@ -4,6 +4,7 @@ import { getUserOrders } from '../../functions/userFunctions';
 import { useSelector } from 'react-redux';
 import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import { formatDateTime } from '../../utils/dateFormatter';
+import PaymentInfo from '../../components/cards/PaymentInfo';
 
 const UserHistory = () => {
 
@@ -58,7 +59,7 @@ const UserHistory = () => {
 
   const showOrders = () => orders.map((order, i) => (
     <div key={i} className='m-5 p-3 card shadow'style={{ boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)' }}>
-      <p>payment info</p>
+      <PaymentInfo order={order} />
       {showOrdersTable(order)}
       <div className='row'>
         <div className='col'>
